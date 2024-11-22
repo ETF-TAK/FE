c44fa35f88886dede1dca26b9d519465d6e0eff8
ZZZINU <nnjin987@gmail.com>
2024-11-22 10:47:02 +0900

    feat(#3): 투자 결과 페이지 UI 구현

--- src/components/etf/etfProfile/ETFProfile.jsx
+++ src/components/etf/etfProfile/ETFProfile.jsx
@@ -3,12 +3,12 @@ import KoreaIcon from '../../../assets/images/common/sectors/korea.png';
 import './style.css';
 
 export default function ETFProfile(props) {
-  const { etfData, isSelected, size, fontsize } = props;
+  const { etfData, isSelected, size, fontsize, color } = props;
 
   return (
     <div className="etf-profile-wrapper">
       <img className="etf-profile-img" src={KoreaIcon} alt="sector 아이콘" style={{ width: size, height: size }} />
-      <div className={`etf-profile-name ${isSelected ? 'selected' : ''}`} style={{ fontSize: fontsize }}>
+      <div className={`etf-profile-name ${isSelected ? 'selected' : ''}`} style={{ fontSize: fontsize, color: color }}>
         {etfData.name}
       </div>
       <div className="etf-profile-company" style={{ fontSize: fontsize }}>
--- src/pages/investPage/investResult/InvestResult.jsx
+++ src/pages/investPage/investResult/InvestResult.jsx
@@ -1,6 +1,63 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './style.css';
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+
+// import required modules
+import { EffectCards } from 'swiper/modules';
+import ETFProfile from '../../../components/etf/etfProfile/ETFProfile';
+import { Link } from 'react-router-dom';
+
 export default function InvestResult() {
-  return <div>InvestResult</div>;
+  const etfData = [
+    {
+      id: 1,
+      name: '한중반도체(합성)',
+      company: 'Kodex',
+    },
+    {
+      id: 2,
+      name: '미국 기술주 ETF',
+      company: 'TIGER',
+    },
+    {
+      id: 3,
+      name: 'S&P 500 인버스',
+      company: 'ARIRANG',
+    },
+    {
+      id: 4,
+      name: '한중반도체(합성)',
+      company: 'Kodex',
+    },
+  ];
+
+  return (
+    <div className="invest-result-wrapper">
+      <div className="invest-result-title">
+        총 13,234,123원의 <br />
+        수익금을 받았을거예요!
+      </div>
+      <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]} className="mySwiper">
+        {etfData.map((etf) => (
+          <SwiperSlide>
+            <div className="invest-result-card-container">
+              <ETFProfile etfData={etf} color="white" />
+              <div className="invest-result-card-amount">3,234,123원</div>
+              <div className="invest-result-card-rate">수익률 +48%</div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <Link to={'/compare'} className="invest-result-compare-btn">
+        ETF 비교하러가기
+      </Link>
+    </div>
+  );
 }
