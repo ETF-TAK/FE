9d86f0a85042c9e4b7963dcba340efa08f848d2c
ZZZINU <nnjin987@gmail.com>
2024-11-23 18:53:16 +0900

    feat(#14): ETF MBTI 테스트 인트로 페이지 버튼 함수 연결

--- src/components/common/button/Button.jsx
+++ src/components/common/button/Button.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import "./style.css";
 
 export default function Button(props) {
-  const { text, fontsize, width } = props;
+  const { text, fontsize, width, onClickFunction } = props;
   return (
     <div>
-      <button className="blue-button" style={{ width: width, fontSize: fontsize }}>
+      <button
+        className="blue-button"
+        style={{ width: width, fontSize: fontsize }}
+        onClick={() => {
+          onClickFunction();
+        }}
+      >
         {text}
       </button>
     </div>
