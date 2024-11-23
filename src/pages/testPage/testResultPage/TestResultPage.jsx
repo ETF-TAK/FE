488ec68fd0bf18dcf8ea1946bfa0d57ad5e4f76b
ZZZINU <nnjin987@gmail.com>
2024-11-23 22:21:13 +0900

    feat(#14): ETF MBTI 테스트 카테고리 카드 컴포넌트 생성

--- src/pages/testPage/testResultPage/TestResultPage.jsx
+++ src/pages/testPage/testResultPage/TestResultPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 import { useLocation } from "react-router-dom";
 import MbtiCard from "../../../components/common/test/mbtiCard/MbtiCard";
+import CategoryCard from "../../../components/common/test/categoryCard/CategoryCard";
 
 export default function TestResultPage() {
   const location = useLocation();
@@ -10,11 +11,17 @@ export default function TestResultPage() {
 
   return (
     <div>
-      <MbtiCard index={0} />
+      {/* <MbtiCard index={0} />
       <MbtiCard index={1} />
       <MbtiCard index={2} />
       <MbtiCard index={3} />
-      <MbtiCard index={4} />
+      <MbtiCard index={4} /> */}
+      <CategoryCard index={0} />
+      <CategoryCard index={1} />
+      <CategoryCard index={2} />
+      <CategoryCard index={3} />
+      <CategoryCard index={4} />
+      <CategoryCard index={5} />
     </div>
   );
 }
