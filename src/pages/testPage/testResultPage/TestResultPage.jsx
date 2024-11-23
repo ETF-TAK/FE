ce440469effc22029f962417013a377d867cfe8c
ZZZINU <nnjin987@gmail.com>
2024-11-23 21:43:15 +0900

    feat(#14): mbti card 컴포넌트 생성

--- src/pages/testPage/testResultPage/TestResultPage.jsx
+++ src/pages/testPage/testResultPage/TestResultPage.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./style.css";
 import { useLocation } from "react-router-dom";
+import MbtiCard from "../../../components/common/test/mbtiCard/MbtiCard";
 
 export default function TestResultPage() {
   const location = useLocation();
   const { answers } = location.state || { answers: [] };
   console.log(location.state);
 
-  return <div>TestResultPage</div>;
+  return (
+    <div>
+      <MbtiCard index={0} />
+      <MbtiCard index={1} />
+      <MbtiCard index={2} />
+      <MbtiCard index={3} />
+      <MbtiCard index={4} />
+    </div>
+  );
 }
