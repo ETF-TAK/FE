c1e7f2d9ea90c8cfe3950a275df3911ae0011298
sinjeong-in <shjin4667@gmail.com>
2024-11-27 13:06:50 +0900

    feat(#6): 주석

--- src/pages/guidePage/guidePages/GuidePage.jsx
+++ src/pages/guidePage/guidePages/GuidePage.jsx
@@ -13,7 +13,7 @@ import wordImg8 from "../../../assets/images/word/word_card8.svg";
 import wordImg9 from "../../../assets/images/word/word_card9.svg";
 import wordImg10 from "../../../assets/images/word/word_card10.svg";
 
-//
+//가이드
 export default function GuidePage() {
   const navigate = useNavigate();
 
