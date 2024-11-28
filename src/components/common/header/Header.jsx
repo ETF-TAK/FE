a6590ec764722c1673828cff21361b7c62b63af1
ZZZINU <nnjin987@gmail.com>
2024-11-28 17:41:42 +0900

    feat(#36): NavBar 드롭다운 추가

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-import MainLogo from '../../../assets/images/common/logo.png';
-import { Link } from 'react-router-dom';
-import './style.css';
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import MainLogo from "../../../assets/images/common/logo.png";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 export default function Header() {
   const location = useLocation();
+  const [isOpenDropdown, setIsOpenDropdown] = useState(false);
 
   return (
     <nav className="navbar">
@@ -14,24 +15,40 @@ export default function Header() {
       </Link>
       <ul className="navbar-links">
         <li>
-          <Link to="/invest" className={location.pathname.startsWith('/invest') ? 'active' : ''}>
+          <Link to="/invest" className={location.pathname.startsWith("/invest") ? "active" : ""}>
             1년 전에 투자했다면?
           </Link>
         </li>
         <li>
-          <Link to="/compare" className={location.pathname.startsWith('/compare') ? 'active' : ''}>
+          <Link to="/compare" className={location.pathname.startsWith("/compare") ? "active" : ""}>
             ETF 비교
           </Link>
         </li>
         <li>
-          <Link to="/guide" className={location.pathname.startsWith('/guide') ? 'active' : ''}>
-            ETF 투자 가이드
+          <Link to="/etf/list" className={location.pathname.startsWith("/etf/list") ? "active" : ""}>
+            섹터별 ETF
           </Link>
         </li>
-        <li>
-          <Link to="/test" className={location.pathname.startsWith('/test') ? 'active' : ''}>
-            투자 성향 테스트
-          </Link>
+
+        {/* 드롭다운 */}
+        <li className="dropdown">
+          <a
+            className={location.pathname.startsWith("/guide") || location.pathname.startsWith("/test") ? "active" : ""}
+          >
+            <p>ETF 알아보기</p>
+          </a>
+          <ul className="dropdown-menu">
+            <li>
+              <Link to="/guide" className={location.pathname.startsWith("/guide") ? "active" : ""}>
+                ETF 투자 가이드
+              </Link>
+            </li>
+            <li>
+              <Link to="/test" className={location.pathname.startsWith("/test") ? "active" : ""}>
+                투자 성향 테스트
+              </Link>
+            </li>
+          </ul>
         </li>
       </ul>
     </nav>
--- src/routers/main-router.jsx
+++ src/routers/main-router.jsx
@@ -1,16 +1,16 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Layout from '../components/common/Layout';
-import MainPage from '../pages/MainPage';
-import InvestPage from '../pages/investPage/InvestPage';
-import ComparePage from '../pages/ComparePage';
-import GuidePage from '../pages/guidePage/guidePages/GuidePage';
-import TestPage from '../pages/testPage/TestPage';
-import InvestAmountPage from '../pages/investPage/investAmountPage/InvestAmountPage';
-import InvestResult from '../pages/investPage/investResult/InvestResult';
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../components/common/Layout";
+import MainPage from "../pages/MainPage";
+import InvestPage from "../pages/investPage/InvestPage";
+import ComparePage from "../pages/ComparePage";
+import GuidePage from "../pages/guidePage/guidePages/GuidePage";
+import TestPage from "../pages/testPage/TestPage";
+import InvestAmountPage from "../pages/investPage/investAmountPage/InvestAmountPage";
+import InvestResult from "../pages/investPage/investResult/InvestResult";
 import DetailPage from "../pages/DetailPage";
-import GuideDetailPage from '../pages/guidePage/guideDetalPage/GuideDetailPage';
+import GuideDetailPage from "../pages/guidePage/guideDetalPage/GuideDetailPage";
 import TestResultPage from "../pages/testPage/testResultPage/TestResultPage";
-
+import ListPage from "../pages/listPage/ListPage";
 
 export const mainRoutes = [
   {
@@ -18,15 +18,16 @@ export const mainRoutes = [
     element: <Layout />,
     children: [
       { element: <MainPage />, index: true },
-      { element: <InvestPage />, path: 'invest' },
-      { element: <InvestAmountPage />, path: 'invest/amount' },
-      { element: <InvestResult />, path: 'invest/result' },
-      { element: <ComparePage />, path: 'compare' },
-      { element: <GuidePage />, path: 'guide' },
-      { element: <GuideDetailPage />, path: 'guide/detail/:num' },
+      { element: <InvestPage />, path: "invest" },
+      { element: <InvestAmountPage />, path: "invest/amount" },
+      { element: <InvestResult />, path: "invest/result" },
+      { element: <ComparePage />, path: "compare" },
+      { element: <GuidePage />, path: "guide" },
+      { element: <GuideDetailPage />, path: "guide/detail/:num" },
       { element: <DetailPage />, path: "compare/detail" },
       { element: <TestPage />, path: "test" },
       { element: <TestResultPage />, path: "test/result" },
+      { element: <ListPage />, path: "etf/list" },
     ],
   },
 ];
