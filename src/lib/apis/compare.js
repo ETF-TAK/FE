9e21026903053a78b8b46b3b37c577504aacfe42
ZZZINU <nnjin987@gmail.com>
2024-12-03 20:35:21 +0900

    feat(#60): ETF 비교하기 목록 조회 API 연결

---
+++ src/lib/apis/compare.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const getCompareETFList = async (filter) => {
+  try {
+    const response = await axios.get("http://localhost:8080/api/compare/result", {
+      params: {
+        filter: filter,
+      },
+    });
+    console.log(response.data);
+
+    return response.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
