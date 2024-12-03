bd819d977f913942dc0f09d40b16f1818b4d9bd7
ZZZINU <nnjin987@gmail.com>
2024-12-04 00:29:33 +0900

    feat(#60): 원형 없는 아이콘 Sector Mapper 생성

--- src/lib/apis/compare.js
+++ src/lib/apis/compare.js
@@ -14,3 +14,18 @@ export const getCompareETFList = async (filter) => {
     console.error(e);
   }
 };
+
+export const postCompareETF = async (filter) => {
+  try {
+    const response = await axios.get("http://localhost:8080/api/compare/result", {
+      params: {
+        filter: filter,
+      },
+    });
+    console.log(response.data);
+
+    return response.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
