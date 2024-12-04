b17596f6b51e3a65d0d2bf650f1646fcaf51c0d7
ZZZINU <nnjin987@gmail.com>
2024-12-04 14:17:31 +0900

    feat(#79): 페이지 애니메이션 추가

--- src/pages/guidePage/guideDetalPage/GuideDetailPage.jsx
+++ src/pages/guidePage/guideDetalPage/GuideDetailPage.jsx
@@ -1,44 +1,41 @@
 import { useParams } from "react-router-dom";
-import './style.css'
-import GuideData from "./guideData"
+import "./style.css";
+import GuideData from "./guideData";
 
-export default function GuideDetailPage(){
-    const { num } = useParams()
-    const content = GuideData[num] || null
+export default function GuideDetailPage() {
+  const { num } = useParams();
+  const content = GuideData[num] || null;
 
-    return (
-        <div>
-            <div className="cardDetail"
-            style={{ backgroundImage: `url(${content.image})`}}/>
-            <div className="etf-detail-title">{content.content.heading}</div>
-            <div className="etf-detail-content">
-                {content.content.paragraphs.map((paragraph, index) => {
-                    if (paragraph.text && paragraph.text.trim() !== "") {
-                        return (
-                            <p key={index} className={paragraph.className}>
-                                {paragraph.text}
-                            </p>
-                        );
-                    } 
-                    if (paragraph.text === "") {
-                        return <div key={index} className="empty-line" />; 
-                    }
-                    if (paragraph.image) {
-                        return (
-                            <div className="image-container" key={index}>
-                                <img
-                                    src={paragraph.image}
-                                    alt={`Paragraph Illustration ${index}`}
-                                    style={{ height: paragraph.height }}
-                                />
-                            </div>
-                        );
-                    }
-                    return null;
-                }   
-                )}
-            </div>
-            
-        </div>
-    )
+  return (
+    <div className="cardDetail-wrapper">
+      <div className="cardDetail" style={{ backgroundImage: `url(${content.image})` }} />
+      <div className="etf-detail-title">{content.content.heading}</div>
+      <div className="etf-detail-content">
+        {content.content.paragraphs.map((paragraph, index) => {
+          if (paragraph.text && paragraph.text.trim() !== "") {
+            return (
+              <p key={index} className={paragraph.className}>
+                {paragraph.text}
+              </p>
+            );
+          }
+          if (paragraph.text === "") {
+            return <div key={index} className="empty-line" />;
+          }
+          if (paragraph.image) {
+            return (
+              <div className="image-container" key={index}>
+                <img
+                  src={paragraph.image}
+                  alt={`Paragraph Illustration ${index}`}
+                  style={{ height: paragraph.height }}
+                />
+              </div>
+            );
+          }
+          return null;
+        })}
+      </div>
+    </div>
+  );
 }
